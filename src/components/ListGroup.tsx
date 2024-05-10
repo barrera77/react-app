955d64adde82f0b1697942a88d22bf7011554666
Manuel Alva <barrera_ml@hotmail.com>
2024-05-09 23:24:37 -0600

    Initial Commit

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,27 @@
+function ListGroup() {
+  let items = [
+    "New York",
+    "Odessa",
+    "Tejocotillos",
+    "La Manga",
+    "Santa Maria Tomatlan",
+  ];
+
+  items = [];
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No items found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
